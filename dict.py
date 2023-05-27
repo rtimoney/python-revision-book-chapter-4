7fc26e43fa10360e234f5e518fc3299ca4e4c17d
rtimoney <103318517+rtimoney@users.noreply.github.com>
2023-05-27 16:07:04 +0100

    Update dict.py

--- dict.py
+++ dict.py
@@ -3,7 +3,7 @@
 # In Python a Dictionary is a data container that can store multiple items of data as a list
 # .... of Key:value pairs  
 # elements are referenced by their key rather than an index number
-# essentially Pythons version of an associative array
+# essentially Pythons version of an associative array 
 
 info = {'name':'Bob','ref':'Python','sys':'Win'}
 print('info :' , type(info))
